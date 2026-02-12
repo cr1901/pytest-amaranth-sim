1054cc00e914a6a39f695be322f0e9480b416ecd
William D. Jones <thor0505@comcast.net>
2026-02-11 19:56:10 -0500

    Use Sybil to check docs in place of pytest-sphinx.

---
+++ conftest.py
@@ -0,0 +1,29 @@
+from sybil import Sybil
+from sybil.parsers.myst import (
+    DocTestDirectiveParser as MarkdownDocTestParser,
+    PythonCodeBlockParser as MarkdownPythonCodeBlockParser
+)
+from sybil.parsers.rest import (
+    DocTestParser as ReSTDocTestParser,
+    PythonCodeBlockParser as ReSTPythonCodeBlockParser
+)
+
+
+markdown_examples = Sybil(
+    parsers=[
+        MarkdownDocTestParser(),
+        MarkdownPythonCodeBlockParser(),
+    ],
+    patterns=['*.md'],
+)
+
+rest_examples = Sybil(
+    parsers=[
+        ReSTDocTestParser(),
+        ReSTPythonCodeBlockParser(),
+    ],
+    patterns=['*.py'],
+)
+
+
+pytest_collect_file = (markdown_examples + rest_examples).pytest()
